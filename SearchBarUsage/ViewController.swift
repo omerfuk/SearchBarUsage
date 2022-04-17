acefa33e983ea3ff1a01c540d90e0d67d1a5873e
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-04-17 14:46:59 +0300

    Initial Commit

---
+++ SearchBarUsage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchBarUsage
+//
+//  Created by Ömer Faruk Kılıçaslan on 17.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
