3f83155e74eecc907690ad27ac768455f7df502e
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-04-17 14:59:07 +0300

    .

--- SearchBarUsage/ViewController.swift
+++ SearchBarUsage/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.searchBar.delegate = self
+        
+        searchBar.scopeButtonTitles = ["Birinci", "İkinci"]
+
     }
 
 
 }
 
+extension ViewController:UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("Arama Sonuç : \(searchText)")
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        print("Cancel Seçildi")
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        if selectedScope == 0{
+            print("Birinci Seçildi")
+        }
+        if selectedScope == 1{
+            print("İkinci Seçildi")
+        }
+    }
+}
